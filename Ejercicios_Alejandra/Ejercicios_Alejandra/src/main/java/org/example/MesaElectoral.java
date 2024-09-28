3e354a6954d61e430c8b490f65d13bf2850ade26
DESKTOP\Alejandra <alevicampos@hotmail.com>
2024-09-28 19:43:41 +0200

    Solucionar problema números negarivos

--- Ejercicios_Alejandra/Ejercicios_Alejandra/src/main/java/org/example/MesaElectoral.java
+++ Ejercicios_Alejandra/Ejercicios_Alejandra/src/main/java/org/example/MesaElectoral.java
@@ -1,32 +1,56 @@
 package org.example;
 
 
+    import java.util.InputMismatchException;
     import java.util.Scanner;
 
     public class MesaElectoral {
         public void mesaelectoral() {
-            Scanner entrada= new Scanner(System.in);
+            Scanner entrada = new Scanner(System.in);
 
             String nombre;
             int edad;
-            int mayorEdad=18;
+            int mayorEdad = 18;
             int diferencia;
+//            boolean error = true;
 
             System.out.println("¡BIENVENIDO A MI MESA ELECTORAL!");
             System.out.println("Introduce tu nombre: ");
-            nombre= entrada.next();
-            System.out.println("Hola " +nombre+". Introduce tu edad:" );
-            edad= entrada.nextInt();
-            diferencia= mayorEdad-edad;
+            nombre = entrada.next();
+            System.out.println("Hola " + nombre + ". Introduce tu edad:");
+            edad = entrada.nextInt();
+            diferencia = mayorEdad - edad;
+//            while (error == true) {
+//                try {
+//                    System.out.println("Hola " + nombre + ". Introduce tu edad:");
+//                    edad = entrada.nextInt();
+//                    diferencia = mayorEdad - edad;
+//
+//                    error = false;
+//
+//                } catch (InputMismatchException Er) {
+//                    System.out.println("El valor introducido no es válido");
+//                    entrada.nextLine();
+//                }
+//            }
 
-            if (edad>= mayorEdad){
-                System.out.println("Enhorabuena " +nombre+ ". Puedes votar. ");
+            while (edad < 0) {
+                System.out.println("ERROR: El valor introducido no es válido");
+                System.out.println("Introduce correctamente tu edad:");
+                edad = entrada.nextInt();
 
-            }else{
-                System.out.println("Lo siento " +nombre+ ". No puedes votar.");
-                System.out.println("Te faltan " +diferencia+ " años para ser mayor de edad.");
             }
 
+                if (edad >= mayorEdad) {
+                    System.out.println("Enhorabuena " + nombre + ". Puedes votar. ");
+
+                } else {
+                    System.out.println("Lo siento " + nombre + ". No puedes votar.");
+                    System.out.println("Te faltan " + diferencia + " años para ser mayor de edad.");
+
+
+                }
+            }
 
         }
-}
+
